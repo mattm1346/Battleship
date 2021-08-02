7b9e852c5b3b85af851f76f16fb0c04b9ec113f9
mattm1346 <MATTM1346@gmail.com>
2021-08-02 14:13:03 +0000

    Add user input for guess and check if input is valid

--- run.py
+++ run.py
@@ -5,24 +5,44 @@ print('Please select difficulty level (easy, medium, hard)')
 print('easy = 1 ship and 6 guesses')
 print('medium = 2 ships 9 guesses')
 print('hard = 3 ships 10 guesses')
+# Create variable for selecting level
 # Show board after difficulty selected
 print('   0 1 2 3 4 5 6 7 8 9')
-# Create vairables for hit, miss and sink
+# Create function to show board
+def show_board(hit, miss, sink):
+    # Create loop for board
+    place = 0
+    for x in range(10):
+        row = ""
+        for y in range(10):
+            character = " _"
+            if place in miss:
+                character = " x"
+            elif place in hit:
+                character = " o"
+            elif place in sink:
+                character = " O"
+            row = row + character
+            place = place + 1
+        print(x, row)
+# Add input for user guess
+def guess():
+    # Create while loop so guess will run until input is valid
+    loop = 'no'
+    while loop == 'no':
+        shot = input("Please enter your guess between 0 and 99 ")
+        # Change shot to int as user input will be number
+        shot = int(shot)
+        # Create if statement checking that guess input is valid 
+        # (between 0 and 99)
+        if shot < 0 or shot > 99:
+            print("Sorry, that number is not on the board. Please try again")
+        else:
+            shot = 'yes'
+            break
+    return shot
 hit = []
 miss = []
 sink = []
-place = 0
-# Create loop for board
-for x in range(10):
-    row = ""
-    for y in range(10):
-        character = " _"
-        if place in miss:
-            character = " x"
-        elif place in hit:
-            character = " o"
-        elif place in sink:
-            character = " O"
-        row = row + character
-        place = place + 1
-    print(x, row)
+shot = guess()
+show_board(hit, miss, sink)
