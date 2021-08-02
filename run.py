f75e9a809192031ad4de758603b03360f0d6446e
mattm1346 <MATTM1346@gmail.com>
2021-08-02 12:16:28 +0000

    Add loop displaying board

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
-print('Welcome to Battleships')
-print('______________________')
+print('Welcome to Battleships!')
+print('_______________________')
 # Add input choosing level difficulty
 print('Please select difficulty level (easy, medium, hard)')
 print('easy = 1 ship and 6 guesses')
@@ -9,5 +9,9 @@ print('hard = 3 ships 10 guesses')
 print('   0 1 2 3 4 5 6 7 8 9')
 # Create loop for board
 for x in range(10):
-    print(x, " _"*10)
+    row = ""
+    for y in range(10):
+        character = " _"
+        row = row + character
+    print(x, row)
 # Add input for guess
