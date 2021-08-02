e28bb7858c9b37c444c5138b1d7ad8dd0e2bc036
mattm1346 <MATTM1346@gmail.com>
2021-08-02 12:01:30 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
-# python code goes here
+print('Welcome to Battleships')
+print('______________________')
+# Add input choosing level difficulty
+print('Please select difficulty level (easy, medium, hard)')
+print('easy = 1 ship and 6 guesses')
+print('medium = 2 ships 9 guesses')
+print('hard = 3 ships 10 guesses')
+# Show board after difficulty selected
+print('   0 1 2 3 4 5 6 7 8 9')
+# Create loop for board
+for x in range(10):
+    print(x, " _"*10)
+# Add input for guess
