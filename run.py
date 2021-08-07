adda1477c79f9a9c992fee72660706107a779b55
mattm1346 <MATTM1346@gmail.com>
2021-08-07 14:43:52 +0000

    Add art from ASCII to beginning of code and reorder guess function and show_board function

--- run.py
+++ run.py
@@ -1,17 +1,40 @@
 print('Welcome to Battleships!')
-print('_______________________')
+print('''                __/___            
+          _____/______|           
+  _______/_____\_______\_____     
+  \              < < <       |    
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~''')
 # Add input choosing level difficulty
-print('Please select difficulty level (easy, medium, hard)')
-print('easy = 1 ship and 6 guesses')
-print('medium = 2 ships 9 guesses')
-print('hard = 3 ships 10 guesses')
+# print('Please select difficulty level (easy, medium, hard)')
+# print('easy = 1 ship and 6 guesses')
+# print('medium = 2 ships 9 guesses')
+# print('hard = 3 ships 10 guesses')
 # Create variable for selecting level
-# Show board after difficulty selected
-print('   0 1 2 3 4 5 6 7 8 9')
+
+# Add input for user guess
+def guess(guesses):
+    # Create while loop so guess will run until input is valid
+    loop = 'no'
+    while loop == 'no':
+        shot = input("Please enter your guess between 0 and 99: \n")
+        # Change shot to int as user input will be number
+        shot = int(shot)
+        # Create if statement checking that guess input is valid.
+        # (between 0 and 99)
+        if shot < 0 or shot > 99:
+            print("Sorry, that number is not on the board. Please try again")
+        elif shot in guesses:
+            print("Sorry, you've used that number before. Try another")
+        else:
+            shot = 'yes'
+            break
+
+    return shot
 
 
 # Create function to show board
 def show_board(hit, miss, sink):
+    print('   0 1 2 3 4 5 6 7 8 9')
     # Create loop for board
     place = 0
     for x in range(10):
@@ -29,30 +52,9 @@ def show_board(hit, miss, sink):
         print(x, row)
 
 
-# Add input for user guess
-def guess():
-    # Create while loop so guess will run until input is valid
-    loop = 'no'
-    while loop == 'no':
-        shot = input("Please enter your guess between 0 and 99: \n")
-        # Change shot to int as user input will be number
-        shot = int(shot)
-        # Create if statement checking that guess input is valid.
-        # (between 0 and 99)
-        if shot < 0 or shot > 99:
-            print(
-                '''Sorry, that number is not on the board.
-                Please try again'''
-                )
-        else:
-            shot = 'yes'
-            break
-        print("Invalid input, please try again")
-    return shot
-
-
 hit = []
 miss = []
 sink = []
-shot = guess()
+guesses = hit + miss + sink
+shot = guess(guesses)
 show_board(hit, miss, sink)
