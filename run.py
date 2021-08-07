8e1fb7f79276f9fd7cb4c7f80418ab603a7f8626
mattm1346 <MATTM1346@gmail.com>
2021-08-07 14:17:40 +0000

    Remove try and except to guess loop and fix whitespace errors

--- run.py
+++ run.py
@@ -8,6 +8,8 @@ print('hard = 3 ships 10 guesses')
 # Create variable for selecting level
 # Show board after difficulty selected
 print('   0 1 2 3 4 5 6 7 8 9')
+
+
 # Create function to show board
 def show_board(hit, miss, sink):
     # Create loop for board
@@ -25,28 +27,30 @@ def show_board(hit, miss, sink):
             row = row + character
             place = place + 1
         print(x, row)
+
+
 # Add input for user guess
 def guess():
     # Create while loop so guess will run until input is valid
     loop = 'no'
     while loop == 'no':
-        try:
-            shot = input("Please enter your guess between 0 and 99: \n")
-            # Change shot to int as user input will be number
-            shot = int(shot)
-            # Create if statement checking that guess input is valid.
-            # (between 0 and 99)
-            if shot < 0 or shot > 99:
-                print(
-                    '''Sorry, that number is not on the board.
-                    Please try again'''
-                    )
-            else:
-                shot = 'yes'
-                break
-        except:
-            print("Invalid input, please try again")
+        shot = input("Please enter your guess between 0 and 99: \n")
+        # Change shot to int as user input will be number
+        shot = int(shot)
+        # Create if statement checking that guess input is valid.
+        # (between 0 and 99)
+        if shot < 0 or shot > 99:
+            print(
+                '''Sorry, that number is not on the board.
+                Please try again'''
+                )
+        else:
+            shot = 'yes'
+            break
+        print("Invalid input, please try again")
     return shot
+
+
 hit = []
 miss = []
 sink = []
